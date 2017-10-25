52873b077b29c4bcf66ec7a778aeed415bf22032
tanirocr@gmail.com <alakazamxdgithub12>
2017-10-24 22:40:12 -0300

    add support do maps

--- app/src/main/java/br/ufrn/eaj/tads/fragmenttabsexample/Fragment3.java
+++ app/src/main/java/br/ufrn/eaj/tads/fragmenttabsexample/Fragment3.java
@@ -1,21 +1,65 @@
 package br.ufrn.eaj.tads.fragmenttabsexample;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * Created by Taniro on 28/09/2016.
  */
 
-public class Fragment3 extends Fragment {
+public class Fragment3 extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap mGoogleMap;
+    MapView mMapView;
+    View mView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View v = inflater.inflate(R.layout.fragment_3, container, false);
+
+        mView = inflater.inflate(R.layout.fragment_3, container, false);
         //logica do fragment
-        return v;
+        return mView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mMapView = (MapView) mView.findViewById(R.id.map);
+
+        if(mMapView != null){
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        MapsInitializer.initialize(getContext());
+        mGoogleMap = googleMap;
+
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(40.689247, -74.044502)).title("Liberdade").snippet("hmm"));
+
+        CameraPosition liberty = CameraPosition.builder().target(new LatLng(40.689247, -74.044502)).zoom(16).bearing(0).build();
+
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(liberty));
+
+
     }
 }
